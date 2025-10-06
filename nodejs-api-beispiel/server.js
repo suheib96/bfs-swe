5fa11877b716b513127f92ef24d38e635d2525b5
Suheib Marzouka <suheibmarzouka@Mac.fritz.box>
2025-10-06 19:33:42 +0200

    add sqlite as db with 3 api routes

--- nodejs-api-beispiel/server.js
+++ nodejs-api-beispiel/server.js
@@ -1,29 +1,55 @@
-const express = require("express");
+const express = require("express"); // import express
 const app = express();
+const sqlite3 = require("sqlite3");
 
+const db = new sqlite3.Database("mydb.db");
 
-app.use(express.json())
-const products = [
-    {id: 1, name: "Produkt 1"},
-    {id: 2, name: "Produkt 2"},
-    {id: 3, name: "Produkt 3"},
-]
-app.get("/products", (req,res) => {
-    res.json(products)
-})
+db.serialize(() => {
+  db.run(
+    "CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, titel TEXT, preis REAL)"
+  );
+  db.run("INSERT INTO products (titel, preis) VALUES ('Produkt 1', 9.99)");
+  db.run("INSERT INTO products (titel, preis) VALUES ('Produkt 2', 19.99)");
+  db.run("INSERT INTO products (titel, preis) VALUES ('Produkt 3', 559.99)");
+  db.run("INSERT INTO products (titel, preis) VALUES ('Produkt 4', 16.49)");
+});
 
-app.get("/products/:id", (req,res) => {
-    const id = req.params.id
-    const product = products.find(p => p.id == id)
-    res.json(product)  
-})
+app.use(express.json()); // Dies ermöglicht uns den Body aus dem Request auszulesen
+// const products = [
+//     {id: 1, name: "Produkt 1"},
+//     {id: 2, name: "Produkt 2"},
+//     {id: 3, name: "Produkt 3"},
+// ]
 
-app.post("/products", (req,res) => {
-    const newProduct = req.body
-    products.push(newProduct)
-    res.json(newProduct)
-})
+app.get("/products", (req, res) => {
+  db.all("SELECT * FROM products", (err, products) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      res.json(products);
+    }
+  });
+});
+
+app.get("/products/:id", (req, res) => {
+  const id = req.params.id;
+  db.get("SELECT * FROM products WHERE id = ?",[id], (err, product) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      res.json(product);
+    }
+  });
+});
+
+app.post("/products", (req, res) => {
+  const {titel,preis} = req.body;
+  db.run("INSERT INTO products (titel,preis) VALUES (?,?)", [titel,preis])
+  res.status(201).send("Produkt erfolgreich erstellt");
+});
 
 app.listen(5006, () => {
-    console.log("Server läuft auf Port 5006")
-})
+  console.log("Server läuft auf Port 5006");
+});
