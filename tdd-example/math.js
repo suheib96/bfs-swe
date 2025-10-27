7aa94baf133394c115730901e9b907a4270872be
Suheib Marzouka <suheibmarzouka@Mac.fritz.box>
2025-10-27 18:29:37 +0100

    fehler korrigiert

--- tdd-example/math.js
+++ tdd-example/math.js
@@ -1,13 +1,11 @@
 function addieren(a,b){
-    return a - b
+    return a + b
 }
 
 function subtrahieren(a,b){
     return a - b
 }
 
-/*************  ✨ Windsurf Command ⭐  *************/
-/*******  f92ff977-059a-4ecd-837b-ec4a4abeda49  *******/
 function dividieren(a,b){
     if(b===0){
         throw new Error("Division durch 0 ist nicht erlaubt!!")
